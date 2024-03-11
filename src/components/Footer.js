833acf7002db70a3e53fc173007afa39a0ddce50
913-Groza-Iulia <iulia.maria.groza@stud.ubbcluj.ro>
2024-03-11 10:18:44 +0200

    App done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,19 @@
-export default function Footer(){
-    return(
-        <div className="footer container">
-            <div className="footer-section">
-                <p className="title">FoodiesHub.com</p>
-                <p>FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free.</p>
-                <p>&copy; 2024 </p>
-            </div>
-            <div className="footer-section">
-                <p className="title">Contact Me</p>
-                <p>groza_iuliabv@yahoo.com</p>
-                <p>+40730556101</p>
-            </div>
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div className="footer container">
+      <div className="footer-section">
+        <p className="title">FoodiesHub.com</p>
+        <p>
+          FoodiesHub is a place where you can please your soul and tummy with
+          delicious food recepies of all cuisine.
+        </p>
+        <p>&copy; 2024 </p>
+      </div>
+      <div className="footer-section">
+        <p className="title">Contact Me</p>
+        <p>groza_iuliabv@yahoo.com</p>
+        <p>+40730556101</p>
+      </div>
+    </div>
+  );
 }
